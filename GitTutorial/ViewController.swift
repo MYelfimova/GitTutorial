2b11955244a794ca0d881c80fe8c88d9acd269bd
Maria Yelfimova <maria.yelfimova@gmail.com>
2020-07-05 10:41:11 +0300

    another print added.

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello there!")
+        print("Hello fo rhte second time!")
     }
 
 
